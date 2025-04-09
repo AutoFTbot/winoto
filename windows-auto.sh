ba1ec5a1d9a23b31e447fc1e30b76ac2ae27a840
AutoFTbot <151357338+AutoFTbot@users.noreply.github.com>
2025-04-10 02:43:51 +0700

    Update windows-auto.sh

--- windows-auto.sh
+++ windows-auto.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Auto Install Windows on VPS with RDP Enabled (Beta)
+#
 
 echo ""
 echo "======================================"
@@ -6,33 +9,29 @@ echo "   Winoto - Auto Install Windows VPS"
 echo "   Created by AutoFTbot - Beta v0.1"
 echo "======================================"
 echo ""
-echo "Silakan pilih OS yang ingin diinstall:"
+echo "Silahkan Pilih OS yang ingin anda install:"
 echo "  1.) Windows 10"
-echo "  2.) Windows Server 2012 R2"
-echo "  3.) Windows Server 2016"
-echo "  4.) Windows Server 2019"
-echo "  5.) Windows Server 2022"
-echo ""
+echo "  2.) Windows 2012 R2"
+echo "  3.) Windows 2016"
+echo "  4.) Windows 2019"
+echo "  5.) Windows 2022"
+read -p "Pilih [1]: " selectos
 
-read -p "Pilih [1-5]: " selectos
-
-# Ethernet default name
 ethernt="Ethernet Instance 0"
 
-# Pilih image
 case "$selectos" in
-  1|"") selectos="https://image.yha.my.id/2:/windows10.gz";;
-  2) selectos="https://image.yha.my.id/2:/windows2012.gz";;
-  3) selectos="https://image.yha.my.id/2:/windows2016.gz";;
-  4) selectos="https://image.yha.my.id/2:/windows2019.gz";;
-  5) selectos="https://image.yha.my.id/2:/windows2022.gz";;
-  *) echo "Pilihan salah!"; exit 1;;
+    1|"") selectos="https://image.yha.my.id/2:/windows10.gz";;
+    2) selectos="https://image.yha.my.id/2:/windows2012.gz";;
+    3) selectos="https://image.yha.my.id/2:/windows2016.gz";;
+    4) selectos="https://image.yha.my.id/2:/windows2019.gz";;
+    5) selectos="https://image.yha.my.id/2:/windows2022.gz";;
+    *) echo "Pilihan salah"; exit;;
 esac
 
 IP4=$(curl -4 -s icanhazip.com)
-GATEWAY=$(ip route | awk '/default/ {print $3}')
+getwey=$(ip route | awk '/default/ { print $3 }')
 
-# Buat file konfigurasi network Windows
+# Buat file net.bat untuk set IP static
 cat >/tmp/net.bat<<EOF
 @ECHO OFF
 cd.>%windir%\\GetAdmin
@@ -42,29 +41,40 @@ echo CreateObject^("Shell.Application"^).ShellExecute "%~s0", "%*", "", "runas",
 del /f /q "%temp%\\Admin.vbs"
 exit /b 2)
 
-netsh -c interface ip set address name="$ethernt" source=static address=$IP4 mask=255.255.255.0 gateway=$GATEWAY
+netsh -c interface ip set address name="$ethernt" source=static address=$IP4 mask=255.255.240.0 gateway=$getwey
 netsh -c interface ip add dnsservers name="$ethernt" address=1.1.1.1 index=1 validate=no
-netsh -c interface ip add dnsservers name="$ethernt" address=8.8.8.8 index=2 validate=no
+netsh -c interface ip add dnsservers name="$ethernt" address=8.8.4.4 index=2 validate=no
 
 ECHO SELECT VOLUME=%%SystemDrive%% > "%SystemDrive%\\diskpart.extend"
 ECHO EXTEND >> "%SystemDrive%\\diskpart.extend"
 START /WAIT DISKPART /S "%SystemDrive%\\diskpart.extend"
 
 cd /d "%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
-
 del "%~f0"
 exit
 EOF
 
-# Tulis image ke /dev/vda
-echo "Mengunduh dan menulis image Windows ke disk..."
+# Buat file rdp-enable.bat untuk aktifkan RDP otomatis
+cat >/tmp/rdp-enable.bat<<EOF
+@echo off
+reg add "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
+cd /d "%ProgramData%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+del "%~f0"
+exit
+EOF
+
+# Install OS Windows ke disk
+echo "[*] Mulai install Windows. Mohon tunggu..."
 wget --no-check-certificate -O- $selectos | gunzip | dd of=/dev/vda bs=3M status=progress
 
-# Mount dan salin file network
-echo "Menyalin konfigurasi network ke Windows..."
+# Mount disk & salin .bat ke Startup Windows
 mount.ntfs-3g /dev/vda2 /mnt
-cp -f /tmp/net.bat "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/Startup/net.bat"
+cd "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/"
+cd Start* || cd start*
+cp -f /tmp/net.bat net.bat
+cp -f /tmp/rdp-enable.bat rdp-enable.bat
 
-echo "Instalasi selesai. Server akan dimatikan dalam 5 detik..."
+echo 'Install selesai. VPS akan mati dalam 5 detik...'
 sleep 5
 poweroff
